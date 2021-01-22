5a4d301e3a59817dbf6c080193f428495bb315b9
Zakk Haug <zakklikeslamps@gmail.com>
2021-01-21 19:21:59 -0500

    scores.js started.

---
+++ Assets/scores.js
@@ -0,0 +1,8 @@
+var showScores = document.getElementById("score-list");
+var clearScores = document.getElementById("clear");
+var returnToQuiz = document.getElementById("return");
+
+clearScores.addEventListener("click", function () {
+
+});
+
